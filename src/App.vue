12328493b7b359c28b59b0af6c01cbed4f571040
Lonk <rinke2005@gmail.com>
2016-05-05 00:42:32 +0200

    Basic Vue.js structure with webpack and socket.io

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello Vue!'
+    }
+  }
+}
+
+
+let socket = io.connect('http://localhost:9000');
+socket.on('connect',() => {
+  console.log('Client has connected to the server!');
+});
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
