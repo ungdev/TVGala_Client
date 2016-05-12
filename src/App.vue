7fbbeee77f3424cfdba4e3782c5809a4114087de
Lonk <rinke2005@gmail.com>
2016-05-12 09:41:30 +0200

    Display SMS

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app" class="display">
     <div class="display-header">
       <div class="display-header-clock">{{ currentDate|hour }}</div>
+      <div class="display-header-sms">Vos messages au<br /><strong>06 52 78 98 62</strong></div>
+      <div class="clear"></div>
     </div>
 
     <div class="display-separator"></div>
@@ -18,6 +20,12 @@
     </div>
 
     <div class="display-sms">
+      <table class="display-sms-message" v-for="message in sms" transition="fade">
+       <tr>
+          <td class="display-sms-message-hour">{{ message.time|hour }}</td>
+          <td class="display-sms-message-content">{{ message.message }}</td>
+        </tr>
+      </table>
     </div>
 
     <div class="clear display-separator"></div>
@@ -37,7 +45,8 @@ export default {
       currentDate: new Date(),
       informations: [],
       information: {message: "Chargement..."},
-      schedules: []
+      schedules: [],
+      sms: []
     }
   },
   methods: {
@@ -70,8 +79,12 @@ export default {
   },
   attached () {
     let socket = io.connect('http://localhost:9000');
-    socket.on('connect',() => {
+    socket.on('connect', () => {
       console.log('Client has connected to the server!');
+    });
+
+    socket.on('sms', data => {
+      this.sms.unshift(data);
     });
 
     socket.on('informations', data => {
@@ -79,7 +92,7 @@ export default {
     });
 
     socket.on('schedules', data => {
-      this.schedules = data.sort(function(a,b){
+      this.schedules = data.sort((a,b) => {
         return new Date(a.start) - new Date(b.start);
       });
     });
@@ -109,6 +122,15 @@ export default {
   text-align:center;
   padding-top:0.4%;
   margin-left:16%;
+  float:left;
+}
+
+.display-header-sms {
+  font-size:3.5vmin;
+  width:18%;
+  text-align:center;
+  margin-right:16%;
+  float:right;
 }
 
 .display-schedules {
@@ -145,6 +167,24 @@ export default {
   width:39%;
   height:71.6%;
   float:right;
+  overflow:hidden;
+}
+
+.display-sms-message {
+  padding:1%;
+  overflow:hidden;
+}
+
+.display-sms-message-hour {
+  font-weight:bold;
+  font-size:2.5vmin;
+  width:10%;
+  border-right:1px double white;
+}
+
+.display-sms-message-content {
+  font-size:2vmin;
+  padding-left:1%;
 }
 
 .display-informations {
@@ -169,11 +209,13 @@ export default {
   clear:both;
 }
 
-.t {
+.fade-transition, .t {
   transition: opacity .4s ease;
 }
-
-.t.fadeout {
+.fade-enter, .t.fadeout {
   opacity: 0;
+}
+.fade-leave {
+  display: none;
 }
 </style>
