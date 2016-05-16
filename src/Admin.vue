670f293be8aefa0006ff42ea59deed2bcf354f1f
Lonk <rinke2005@gmail.com>
2016-05-16 19:15:17 +0200

    Get informations and schedules on admin

--- src/Admin.vue
+++ src/Admin.vue
@@ -8,9 +8,25 @@ import config from './config';
 export default {
   data () {
     return {
+      informations: [],
+      schedules: []
     }
   },
   methods: {
+
+  },
+  attached() {
+    fetch(`http://${config.server.host}:${config.server.port}/informations`)
+      .then(res => res.json())
+      .then(results => {
+        this.informations = results;
+      });
+
+    fetch(`http://${config.server.host}:${config.server.port}/schedules`)
+      .then(res => res.json())
+      .then(results => {
+        this.schedules = results;
+      });
   }
 }
 </script>
